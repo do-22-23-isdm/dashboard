6f6bb11d9e7268876dedc65614c3305b7580647c
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2024-03-03 22:14:21 +0100

    feat(components): add theme switcher to topbar

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,6 +1,7 @@
 import { MainNav } from '@/components/navigation/main-nav';
 import { UserNav } from '@/components/navigation/user-nav';
 import { cn } from '@/lib/utils';
+import { ThemeSwitcher } from './ui-custom/theme-switcher';
 
 type TopbarProps = {} & React.HTMLAttributes<HTMLDivElement>;
 
@@ -15,6 +16,7 @@ export function Topbar({ className }: TopbarProps) {
       <div className="flex h-16 items-center px-4">
         <MainNav className="mx-6" links={links} />
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeSwitcher />
           <UserNav />
         </div>
       </div>
