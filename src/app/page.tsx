d64da2042d926c82cb82d62012964c9e6db855c1
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2024-03-03 18:13:02 +0100

    fix(app): add flex on homepage grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       </div>
       {/* main content */}
       <main className="flex-1 flex">
-        <div className="grid lg:grid-cols-5">
+        <div className="grid lg:grid-cols-5 flex-1">
           <Sidebar className="hidden lg:block" />
           <div className="col-span-3 lg:col-span-4 lg:border-l">
             <div className="h-full px-4 py-6 lg:px-8">{content}</div>
