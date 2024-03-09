1ed8bdf1fdc3584b6d16330e102c0f4e5d4b9c6e
Matéo Fernandez <mateo.fernandez@etu.umontpellier.fr>
2024-03-09 18:44:49 +0100

    fix(i18n): update html metadata + page name in locales json

--- src/app/[locale]/dashboard/alerts/page.tsx
+++ src/app/[locale]/dashboard/alerts/page.tsx
@@ -6,12 +6,21 @@ import React from 'react';
 import { getTranslations } from 'next-intl/server';
 import { AlertList } from '@/components/alerts/alert-list';
 
+export async function generateMetadata() {
+  const t = await getTranslations();
+
+  return {
+    title: `${t('Alerts.title')} - ${t('Metadata.appTitle')}`,
+    description: t('Alerts.description'),
+  };
+}
+
 export default async function Alerts() {
   const t = await getTranslations();
 
   return (
     <>
-      <Header title={t('Alertes.title')} subtitle={t('Alertes.description')} />
+      <Header title={t('Alerts.title')} subtitle={t('Alerts.description')} />
       <Separator className="my-4" />
       <Button className="mb-4" variant="outline">
         <Plus className="mr-2 h-4 w-4" />
@@ -21,22 +30,20 @@ export default async function Alerts() {
         className="grid grid-cols-1 gap-4"
         alerts={[
           {
-            title: t('Alertes.placeholder.clusterReady.title'),
-            description: t('Alertes.placeholder.clusterReady.description'),
+            title: t('Alerts.placeholder.clusterReady.title'),
+            description: t('Alerts.placeholder.clusterReady.description'),
             icon: <BadgeCheck />,
             state: 'success',
           },
           {
-            title: t('Alertes.placeholder.quotaAlmostReached.title'),
-            description: t(
-              'Alertes.placeholder.quotaAlmostReached.description',
-            ),
+            title: t('Alerts.placeholder.quotaAlmostReached.title'),
+            description: t('Alerts.placeholder.quotaAlmostReached.description'),
             icon: <AlertTriangleIcon />,
             state: 'warning',
           },
           {
-            title: t('Alertes.placeholder.quotaReached.title'),
-            description: t('Alertes.placeholder.quotaReached.description'),
+            title: t('Alerts.placeholder.quotaReached.title'),
+            description: t('Alerts.placeholder.quotaReached.description'),
             icon: <Bomb />,
             state: 'error',
           },
