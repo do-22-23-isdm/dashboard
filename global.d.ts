3fbef46060b7854e78d26133cba1cf10a01fd67d
Matéo Fernandez <mateo.fernandez@etu.umontpellier.fr>
2024-03-09 23:21:07 +0100

    fix(vscode): move locales folder to get linting working

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,5 @@
 // Use type safe message keys with `next-intl`
-type DefaultMessages = typeof import('src/locales/en.json');
-type FrMessages = typeof import('src/locales/fr.json');
+type DefaultMessages = typeof import('./locales/en.json');
+type FrMessages = typeof import('./locales/fr.json');
 
 declare interface IntlMessages extends DefaultMessages, FrMessages {}
--- src/i18n.ts
+++ src/i18n.ts
@@ -12,6 +12,6 @@ export default getRequestConfig(async ({ locale }) => {
   }
 
   return {
-    messages: (await import(`./locales/${baseLocale}.json`)).default,
+    messages: (await import(`../locales/${baseLocale}.json`)).default,
   };
 });
