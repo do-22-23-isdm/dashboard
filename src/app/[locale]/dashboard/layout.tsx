a9e00c2bbe010a9d7ffe89225339b5eafaab5e92
Matéo Fernandez <mateo.fernandez@etu.umontpellier.fr>
2024-03-11 12:10:06 +0100

    refactor(sidebar): client component only for Sidebar items

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -1,5 +1,8 @@
 import { getTranslations } from 'next-intl/server';
-import { Sidebar } from '@@/sidebar';
+import { useTranslations } from 'next-intl';
+import { LayoutDashboard, Inbox } from 'lucide-react';
+import { Sidebar, SidebarSection } from '@@/navigation/sidebar/sidebar';
+import { SidebarItem } from '@@/navigation/sidebar/item';
 
 export async function generateMetadata() {
   const t = await getTranslations();
@@ -11,9 +14,24 @@ export async function generateMetadata() {
 }
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const t = useTranslations('Dashboard.Sidebar');
+
   return (
     <div className="grid lg:grid-cols-5 flex-1">
-      <Sidebar className="hidden lg:block" />
+      <Sidebar className="hidden lg:block">
+        <SidebarSection>
+          <SidebarItem
+            title={t('overview')}
+            href={'/dashboard'}
+            icon={<LayoutDashboard />}
+          />
+          <SidebarItem
+            title={t('alerts')}
+            href={'/dashboard/alerts'}
+            icon={<Inbox />}
+          />
+        </SidebarSection>
+      </Sidebar>
       <div className="col-span-3 lg:col-span-4 lg:border-l">
         <div className="h-full px-4 py-6 lg:px-8">{children}</div>
       </div>
